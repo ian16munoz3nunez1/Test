ea2d5484a42cc5d9ec1039389fbe6c098deabc99
Ian Muñoz Nuñez <ian.munoz4644@alumnos.udg.mx>
2020-09-18 13:33:29 -0500

    Se agrego la funcion mostrar_enteros a la opcion 1 y las opciones se leen como caracteres y no como enteros

--- main.c
+++ main.c
@@ -2,11 +2,12 @@
 #include<stdlib.h>
 #include<conio.h>
 #include<string.h>
+#define t 5
 void capturar_enteros();
 void mostrar_cadena(int, char *);
 int main()
 {
-    int opc;
+    char opc;
     do
     {
         printf("Elige una opcion...\n\n");
@@ -15,18 +16,43 @@ int main()
         printf("2. Imprimir una cadena n veces\n");
         printf("3. Capturar datos de personajes\n");
         printf("4. Mostrar datos de personajes\n");
-        scanf("%i", &opc);
+        fflush(stdin);
+        scanf("%c", &opc);
+        system("cls");
         switch(opc)
         {
-        case 1:
-            break;
-        case 2:
-            break;
-        case 3:
-            break;
-        case 4:
-            break;
+        case '1':
+            capturar_enteros();
+        break;
+        case '2':
+        break;
+        case '3':
+        break;
+        case '4':
+        break;
         }
+        system("pause");
+        system("cls");
     }
-    while(opc != 0);
+    while(opc != '0');
+}
+void capturar_enteros()
+{
+    size_t i;
+    int arreglo[t], sum;
+    float prom;
+    sum = 0;
+    for(i=0; i<t; i++)
+    {
+        printf("Ingresa el valor %i: ", i+1);
+        scanf("%i", &arreglo[i]);
+        printf("\n");
+        sum += arreglo[i];
+    }
+    prom = sum/t;
+    printf("\nValores: ");
+    for(i=0; i<t; i++)
+        printf("%i ", arreglo[i]);
+    printf("\nSuma: %i", sum);
+    printf("\nPromedio: %.2f\n\n", prom);
 }
